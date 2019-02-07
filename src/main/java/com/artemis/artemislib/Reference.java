7414686c7f6bdf42bd420e35e52209fd48f83e9b
Cammie <inazuma261@gmail.com>
2019-02-07 15:00:42 -0500

    Blep

--- src/main/java/com/artemis/artemislib/Reference.java
+++ src/main/java/com/artemis/artemislib/Reference.java
@@ -7,7 +7,7 @@ public class Reference
 	public static final String NAME = "ArtemisLib";
 	public static final String VERSION = "1.0.0";
 	public static final String ACCEPTEDVERSIONS = "[1.12.2]";
-	public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2774,)";
+	public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2795,)";
 		
 	//Proxies
 	public static final String CLIENT_PROXY_CLASS = "com.artemis.artemislib.proxy.ClientProxy";
--- src/main/java/com/artemis/artemislib/util/AttachAttributes.java
+++ src/main/java/com/artemis/artemislib/util/AttachAttributes.java
@@ -2,28 +2,21 @@ package com.artemis.artemislib.util;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.UUID;
 
 import com.artemis.artemislib.util.attributes.ArtemisLibAttributes;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityAgeable;
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.event.entity.EntityEvent;
-import net.minecraftforge.event.entity.living.LivingDamageEvent;
-import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -35,9 +28,9 @@ public class AttachAttributes
 	@SubscribeEvent
 	public static void attachAttributes(EntityEvent.EntityConstructing event)
 	{
-		if(event.getEntity() instanceof EntityLivingBase)
+		if(event.getEntity() instanceof EntityPlayer)
 		{
-			EntityLivingBase entity = (EntityLivingBase) event.getEntity();
+			EntityPlayer entity = (EntityPlayer) event.getEntity();
 			AbstractAttributeMap map = entity.getAttributeMap();
 			
 			map.registerAttribute(ArtemisLibAttributes.ENTITY_HEIGHT);
@@ -46,26 +39,21 @@ public class AttachAttributes
 	}
 	
 	@SubscribeEvent
-	public static void onEntityTick(LivingEvent.LivingUpdateEvent event)
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event)
 	{
-		EntityLivingBase entity = event.getEntityLiving();
-		double heightAttribute = entity.getAttributeMap().getAttributeInstance(ArtemisLibAttributes.ENTITY_HEIGHT).getAttributeValue();
-		double widthAttribute = entity.getAttributeMap().getAttributeInstance(ArtemisLibAttributes.ENTITY_WIDTH).getAttributeValue();
-		float height = (float) (entity.height * heightAttribute);
-		float width = (float) (entity.width * widthAttribute);
-		AxisAlignedBB aabb = entity.getEntityBoundingBox();
+		EntityPlayer player = event.player;
+		double heightAttribute = player.getAttributeMap().getAttributeInstance(ArtemisLibAttributes.ENTITY_HEIGHT).getAttributeValue();
+		double widthAttribute = player.getAttributeMap().getAttributeInstance(ArtemisLibAttributes.ENTITY_WIDTH).getAttributeValue();
+		float height = (float) (player.height * heightAttribute);
+		float width = (float) (player.width * widthAttribute);
+		AxisAlignedBB aabb = player.getEntityBoundingBox();
 		double d0 = width / 2.0D;
-		
-		if(event.getEntityLiving() instanceof EntityPlayer)
-		{
-			EntityPlayer player = (EntityPlayer) event.getEntityLiving();
-			float eyeHeight = (float) (player.getDefaultEyeHeight() * heightAttribute);
-			player.eyeHeight = eyeHeight;
-		}
+		float eyeHeight = (float) (player.getDefaultEyeHeight() * heightAttribute);
+		player.eyeHeight = eyeHeight;
 		
 		try
 		{
-			setSize.invoke(entity, width, height);
+			setSize.invoke(player, width, height);
 		}
 		catch (IllegalAccessException e)
 		{
@@ -80,8 +68,8 @@ public class AttachAttributes
 			e.printStackTrace();
 		}
         
-		entity.setEntityBoundingBox(new AxisAlignedBB(entity.posX - d0, aabb.minY, entity.posZ - d0, 
-				entity.posX + d0, aabb.minY + height, entity.posZ + d0));
+		player.setEntityBoundingBox(new AxisAlignedBB(player.posX - d0, aabb.minY, player.posZ - d0, 
+				player.posX + d0, aabb.minY + height, player.posZ + d0));
 	}
 	
 	@SideOnly(Side.CLIENT)
@@ -103,25 +91,5 @@ public class AttachAttributes
 	public static void onLivingRenderPost(RenderLivingEvent.Post event)
 	{
 		GlStateManager.popMatrix();
-	}
-	
-	@SubscribeEvent
-	public static void onLivingHurt(LivingDamageEvent event)
-	{
-		EntityLivingBase entity = event.getEntityLiving();
-		UUID height = UUID.fromString("5017c7e0-413a-4023-b569-afcf57dbec18");
-		UUID width = UUID.fromString("32b6d7e3-c5ba-4d9b-9244-b706a273c4b3");
-		
-		if(!entity.world.isRemote)
-		{
-			if(event.getSource().isMagicDamage())
-			{
-				Multimap<String, AttributeModifier> attributes = HashMultimap.create();
-				
-				attributes.put(ArtemisLibAttributes.ENTITY_HEIGHT.getName(), new AttributeModifier(height, "Height", 1.0, 2));
-				attributes.put(ArtemisLibAttributes.ENTITY_WIDTH.getName(), new AttributeModifier(width, "Width", 1.0, 2));
-				entity.getAttributeMap().applyAttributeModifiers(attributes);
-			}
-		}
 	}
 }
